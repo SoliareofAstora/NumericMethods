9989eb116425888a1a5ec56c010348cb8522be2f
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-12-03 03:14:38 +0100

    Back to stable algorithm

--- 3diagonalMatrix.py
+++ 3diagonalMatrix.py
@@ -4,19 +4,15 @@ Created on Sat Dec  2 20:58:06 2017
 
 @author: SoliareOfAstora
 """
-import math 
 import numpy as np
 from diagonal import Diagonal
 from GivensRotation import GivensRotations
 
-
-
-#=====Prepare Matrix=========================
+#=Prepare Matrix 9x9=========================
 d1 = Diagonal()
 d1.setNumbers(0, [4, 8, 4, 3, 4, 5, 2, 8, 5])
 d0 = Diagonal()
 d0.setNumbers(-1, [2, 1, 1, 2, 1, 2, 1, 2])
-b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 source = np.zeros((9,9))
 for x in range(9):
@@ -24,23 +20,32 @@ for x in range(9):
     if x>0:
         source[x-1,x]=d0.getNumber(x)
         source[x,x-1]=d0.getNumber(x)
-#============================================
+    
+b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-#=====Correct answer ========================
+#=Calculate correct answer with numpy========
 CorrectSolve = np.linalg.solve(source,b)   
-print("Correct solve")
+print("\nnumpy solve")
 for x in range(9):
     print("x%d"%(x+1),"=%f"%CorrectSolve[x])
-#============================================
 
+#=Calculate GivensRotations==================
+for x in range(np.size(source[0])-1):
+    G = GivensRotations(source,x)
+    source= G.dot(source)
+    b= G.dot(b)
+
+
+result = np.zeros(np.shape(b))
+maxi = np.size(source[0])-1
+result[maxi]=b[maxi]/source[maxi,maxi]
+for x in (np.size(source[0])-1,-1,-1):
+    for y in (np.size(source[0])-1,x,-1):
+        result[x]=b[x]
     
-a = np.copy(source)
 
-G = GivensRotations(a)
-a= G.dot(a)
-b= G.dot(b)
 
-CorrectSolve = np.linalg.solve(a,b)   
+CorrectSolve = np.linalg.solve(source,b)   
 print("Correct solve")
 for x in range(9):
     print("x%d"%(x+1),"=%f"%CorrectSolve[x])
