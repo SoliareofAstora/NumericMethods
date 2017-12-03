a02ff796027a556db0b1158d51fc3251214f3e26
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-12-03 01:25:23 +0100

    OMG I JUST DID IT!!!!!
    To be honest, that was jsut ONE LINE OF CODE!!!

--- 3diagonalMatrix.py
+++ 3diagonalMatrix.py
@@ -8,20 +8,23 @@ from diagonal import Diagonal
 import numpy as np
 import math
 
+def distance(p0, p1):
+    return math.sqrt((p0)**2 + (p1)**2)
+
 def Givens(source,x):
-    r=math.sqrt(math.pow(source[x,x],2)+math.pow(source[x + 1,x],2))
-    c=source[x , x] / r
-    s=source[x + 1,x] / r
-    output = np.zeros(np.shape(source))
-    np.fill_diagonal(output,1)
-    output[x,x]=c
-    output[x + 1, x + 1] = c
-    output[x + 1, x] = -s
-    output[x, x + 1] = s
-    return output
+    (rows, cols)=np.shape(source)
+    
+    r = distance(source[x,x],source[x+1,x])
+    c = source[x,x] / r
+    s = -source[x+1,x] / r
+    
+    G=np.identity(9)
+    G[x, x] = c
+    G[x + 1, x + 1] = c
+    G[x + 1, x] = s
+    G[x, x + 1] = -s
 
-
-
+    return G
 
 #=====Prepare Matrix=========================
 d1 = Diagonal()
@@ -29,20 +32,43 @@ d1.setNumbers(0, [4, 8, 4, 3, 4, 5, 2, 8, 5])
 d0 = Diagonal()
 d0.setNumbers(-1, [2, 1, 1, 2, 1, 2, 1, 2])
 b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
-a = np.zeros((9,9))
+
+source = np.zeros((9,9))
 for x in range(9):
-    a[x,x]=d1.getNumber(x)
+    source[x,x]=d1.getNumber(x)
     if x>0:
-        a[x-1,x]=d0.getNumber(x)
-        a[x,x-1]=d0.getNumber(x)
+        source[x-1,x]=d0.getNumber(x)
+        source[x,x-1]=d0.getNumber(x)
 #============================================
 
-#=====Correct answer 
-CorrectSolve = np.linalg.solve(a,b)   
-
+#=====Correct answer ========================
+CorrectSolve = np.linalg.solve(source,b)   
 print("Correct solve")
 for x in range(9):
     print("x%d"%(x+1),"=%f"%CorrectSolve[x])
-    
+#============================================
 
-#Givens(a,3)
+    
+a = np.copy(source)
+
+
+for x in range(8):
+    G = Givens(a,x)
+    a= G*a
+    b= G.dot(b)
+
+CorrectSolve = np.linalg.solve(a,b)   
+print("Correct solve")
+for x in range(9):
+    print("x%d"%(x+1),"=%f"%CorrectSolve[x])
+
+
+
+
+
+
+
+
+
+
+
