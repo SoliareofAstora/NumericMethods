7ff3b2573527366b9271445624e341d811096698
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-12-25 11:24:16 +0100

    fix

--- Givens_Rotation_Example.py
+++ Givens_Rotation_Example.py
@@ -5,24 +5,21 @@ Created on Sat Dec  2 20:58:06 2017
 @author: SoliareOfAstora
 """
 import numpy as np
-from diagonal import Diagonal
 from GivensRotation import GivensRotations
 from BackSubstitution import backSubstitution
 #=Prepare Matrix 9x9=========================
-d1 = Diagonal()
-d1.setNumbers(0, [4, 8, 4, 3, 4, 5, 3, 8, 5])
-d0 = Diagonal()
-d0.setNumbers(-1, [2, 1, 1, 2, 1, 2, 1, 2])
-
-source = np.zeros((9,9))
-for x in range(9):
-    source[x,x]=d1.getNumber(x)
-    if x>0:
-        source[x-1,x]=d0.getNumber(x)
-        source[x,x-1]=d0.getNumber(x)   
 b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+d2 = np.array([2, 1, 1, 2, 1, 2, 1, 2])
+d1 = np.array([4, 8, 4, 3, 4, 5, 3, 8, 5])
+d0 = np.array([2, 1, 1, 2, 1, 2, 1, 2])
 
 #=Print correct answer with numpy.solve======
+source = np.zeros((9,9))
+for x in range(9):
+    source[x,x]=d1[x]
+    if x>0:
+        source[x-1,x]=d0[x-1]
+        source[x,x-1]=d2[x-1]
 CorrectSolve = np.linalg.solve(source,b)   
 print("\nsource & numpy solve")
 for x in range(9):
