798587db9c3d1b0cf95e23b7a1dd7ed277d88dfe
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-12-29 19:30:40 +0100

    almost done

--- PowerMethod_Example.py
+++ PowerMethod_Example.py
@@ -10,14 +10,31 @@ class diagonalArray:
     values = np.array([8,2,1])
     offset = np.array([0,1,3])
     
-    def getValue(self, x , y):
+    def get(self, x , y):
+        if y<0: 
+            return 0
         distance = x - y;
         if distance < 0: 
             distance *= -1;
         for i in range(np.size(self.offset)):
             if self.offset[i]==distance:
                 return self.values[i]
-        return 0;
+        return 0
         
+    def calculate(self,source,output):
+        for i in range(64):
+            output[i] = 0
+            for j in range(-3,4,1):
+                if (i+j>=0 and i+j<64):
+                    output[i] += self.get(self,i,i+j)*source[i+j]
+            
 arr = diagonalArray
-diagonalArray.getValue(arr,2,2)
+srr = np.ones((64))
+wrr = np.ones((64))
+result = np.zeros((10000,64))
+for i in range(10000):
+    wrr = wrr/np.linalg.norm(wrr)
+    srr=np.copy(wrr)
+    result[i]=np.copy(wrr)
+    diagonalArray.calculate(arr,srr,wrr)
+    
