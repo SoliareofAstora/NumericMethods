96430c37e857600e22fd5d517f5481d7d5d22501
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-01-26 20:41:07 +0100

    FINISHED THIS ALL

--- RosenbrockMinimum.py
+++ RosenbrockMinimum.py
@@ -5,11 +5,11 @@ import random as rand
 
 
 def f(x, y):
-    return (1 - x ** 2) + 100 * (y - x ** 2) ** 2
+    return (1 - x)**2 + 100 * (y - x ** 2) ** 2
 
 
 def fdx(x, y):
-    return 2 * x * (200 * x ** 2 - 200 * y - 1)
+    return 2*(200 * x ** 3 - 200 * x * y + x - 1)
 
 
 def fdy(x, y):
@@ -28,32 +28,35 @@ rng = 2
 X = np.linspace(-rng, rng, 100)
 Y = np.linspace(-rng, rng, 100)
 Z = arrayf(X, Y)
-epsilon = 1e-15
+epsilon = 1e-8
 step = 0.001
-prev = 0.3
+prev = 0.5
 for i in range(20):
 
     xy = np.array([rand.uniform(-rng, rng), rand.uniform(-rng, rng)])
-    history = np.zeros([2, 2002])
+    history = np.empty(2)
     vector = np.array([0,0])
     a = 0
-    history[:, a] = xy
+    history =xy
     while True:
         a += 1
-        vector = step * np.array([fdx(xy[0], xy[1]), fdy(xy[0], xy[1])])+prev *vector
-        xy -= vector
+        vector = np.array([fdx(xy[0], xy[1]), fdy(xy[0], xy[1])])+prev *vector
+        xy -= step*vector
 
-        history[:, a] = xy
+        history = np.append(history, xy)
 
-        # if f(xy[0], xy[1]) < epsilon or
-        if a > 2000:
+        distance = np.linalg.norm(xy-[1,1])
+        if distance < epsilon or distance>100:
             break
 
     plt.pcolor(X, Y, Z, norm=LogNorm(10))
-    plt.colorbar()
-    plt.plot(history[1, :a], history[0, :a], color="red")
-    plt.plot(history[1, 0], history[0, 0], ".")
+
+    plt.plot(history[1::2], history[0::2])#,# color="red")
+    plt.plot(history[1], history[0], ".")
     plt.title(str(a) + " steps  Momentum f(x) = " + str(f(xy[0], xy[1])))
     print(str(f(xy[0], xy[1])))
     plt.savefig('plots/rosenbergMinimalization' + str(i) + '.png')
+
+    plt.colorbar()
     plt.clf()
+
--- lastone.py
+++ lastone.py
@@ -1,13 +1,16 @@
-from numpy.polynomial.laguerre import lagroots
-import numpy as n
+from numpy.polynomial.laguerre import lagroots, Laguerre,poly2lag,lag2poly
+import numpy as np
 
-p = n.poly1d([1, 1, 3, 2, -1, -3, -11, -8, -12, -4, 4])
-roots = lagroots(p)
+p = np.poly1d([1, 1, 3, 2, -1, -3, -11, -8, -12, -4, 4])
+roots = p.roots()
+
+p(-1.33826102e+01)
 
 print(p)
 for i in range(roots.size):
-    print(roots[i])
+    print(p(roots[i]))
 print()
+
 
 p = n.poly1d([1, complex(0, 1), -1, complex(0, -1), 1])
 roots = lagroots(p)
@@ -15,3 +18,9 @@ roots = lagroots(p)
 print(p)
 for i in range(roots.size):
     print(roots[i])
+
+
+
+pp=lag2poly(p)
+lagroots(pp)
+print (p)
